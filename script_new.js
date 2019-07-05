5df074e32fbd59cd25c0720410a8f51687cc6e09
shantanuaryan67 <shantanuaryan67@gmail.com>
2019-07-05 17:44:16 +0530

    5 july
    animated

--- script_new.js
+++ script_new.js
@@ -8,7 +8,8 @@ function colorgen() {
 }
 
 function colorChanger() {
-  document.documentElement.style.setProperty('--customcolor', colorgen());
+  // document.documentElement.style.setProperty('--customcolor', colorgen());
+  document.documentElement.style.setProperty('--customcolor', rand_hsl(0,360,70,90,60,70));
   themechanger();
 }
 
@@ -19,6 +20,16 @@ function getcolorvalue() {
 function themechanger() {
   var metaThemeColor = document.querySelector("meta[name=theme-color]");
   metaThemeColor.setAttribute("content", getcolorvalue());
+}
+
+function rand(min,max) // min and max included
+{
+    return Math.floor((Math.random()*(max-min+1))+min);
+}
+
+function rand_hsl(h1,h2,s1,s2,l1,l2) {
+  hslc = "hsl(" + rand(h1,h2) + "," + rand(s1,s2) + "%," + rand(l1,l2) + "%)";
+  return hslc;
 }
 
 colorChanger();
